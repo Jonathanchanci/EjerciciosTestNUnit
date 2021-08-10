b31c3a25c6192e6077038d85f496a70eeb290ed6
Jonathan <jonathan.a.chanci@gmail.com>
2021-08-09 20:49:14 -0500

    Init Commit

---
+++ TestArrayList/ArrayList.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+using System.Collections;
+
+namespace TestArrayList
+{
+    public class Tests
+    {
+        /*
+         Ejercicio Prueba de ArrayList
+        Escribe al menos una prueba para verificar cada uno de los siguientes comportamientos de la clase ArrayList.
+
+        CuandoSeCreaCountEsCero
+        CuandoSeAgregaUnItemCountEsUno
+        ContainsEsTrueCuandoContieneElItem
+        ContainsEsFalseNoContieneElItem 
+         */
+        public ArrayList array;
+        const string texto = "hola";
+        const string textoNo = "como";
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+            array = new ArrayList();
+            array.Add(texto);
+        }
+
+        [Test]
+        public void CuandoSeCreaCountEsCero()
+        {
+            //Arrange
+            array = new ArrayList();
+
+            //Act
+            int contadorArray = array.Count;
+
+            //Assert
+            Assert.AreEqual(0, contadorArray);
+        }
+
+        [Test]
+        public void CuandoSeAgregaUnItemCountEsUno()
+        {
+            //Act
+            int contadorArray1 = array.Count;
+
+            //Assert
+            Assert.AreEqual(1, contadorArray1);
+        }
+
+        [Test]
+        public void ContainsEsTrueCuandoContieneElItem()
+        {
+            //Act
+            bool loContiene = array.Contains(texto);
+
+            //Arrange
+            Assert.IsTrue(loContiene);
+        }
+
+        [Test]
+        public void ContainsEsFalseNoContieneElItem()
+        {
+            //Act
+            bool loContiene = array.Contains(textoNo);
+
+            //Arrange
+            Assert.IsFalse(loContiene);
+        }
+    }
+}
---
+++ TestArrayList/StackTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System.Collections;
+
+namespace Test
+{
+    /*
+     Ejercicio prueba de stack
+    Un stack (pila) es un estructura de datos que funciona según la regla "ultimo entrado primero salido". 
+    Escribe 3 test para verificar este comportamiento teniendo presente que el método para agregar 
+    ítems es push y el método para retirar items es pop.
+     */
+    public class StackTest
+    {
+        Stack _stack = new Stack();
+        [SetUp]
+        public void SetUp()
+        {
+            _stack.Push(1);
+            _stack.Push(2);
+        }
+
+        [Test]
+        public void UltimoEntraPrimeroSale()
+        {
+            //Act
+            var valor2 = _stack.Pop();
+
+            //Assert
+            Assert.AreEqual(2, valor2);
+        } 
+
+        [Test]
+        public void Entra3Sale3()
+        {
+            //Arrange
+            _stack.Push(3);
+
+            //Act
+            var valor3 = _stack.Pop();
+
+            //Assert
+            Assert.AreEqual(3, valor3);
+        }
+
+        [Test]
+        public void Borro1Sale1()
+        {
+            //Arrange
+            _stack.Pop();
+
+            //Act
+            var valor1 = _stack.Pop();
+
+            //Assert
+            Assert.AreEqual(1, valor1);
+        }
+    }
+}
